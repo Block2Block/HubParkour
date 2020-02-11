541b4777326078392e537c43c42910ea2332abbe
Block2Block <thebarkerkids@hotmail.com>
2020-02-11 16:01:32 +0000

    v2.0.2

--- src/main/java/me/Block2Block/HubParkour/utils/ItemUtil.java
+++ src/main/java/me/Block2Block/HubParkour/utils/ItemUtil.java
@@ -23,7 +23,7 @@ public class ItemUtil {
             sm.setOwner(skullName);
             im = sm;
         }
-        im.spigot().setUnbreakable(true);
+        im.setUnbreakable(true);
         im.addItemFlags(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ATTRIBUTES);
         is.setItemMeta(im);
         return is;
